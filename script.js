4414fe4d9bcf611a5a27c69cc0f29befa06aa6a9
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-21 14:47:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
-const status=getElementById("#status");
-const enterBtn=getElementById("#enterBtn");
+// const status=getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+function meta(){
+	document.getElementById("status").innerHTML = '<h1>Entered Metaverse</h1>'
+}
 
-enterBtn.addEventListener("click",function(){
-	status.innerHTML="Entered Metaverse";
-})
+enterBtn.addEventListener("click",meta)
