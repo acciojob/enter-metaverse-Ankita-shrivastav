bc5b0d8eaabe3d790ff7703ec1005ab1e064f102
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-21 14:31:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-const status=getElementById("status");
-const enterBtn=getElementById("enterBtn");
+const status=getElementById("#status");
+const enterBtn=getElementById("#enterBtn");
 
 enterBtn.addEventListener("click",function(){
 	status.innerHTML="Entered Metaverse";
